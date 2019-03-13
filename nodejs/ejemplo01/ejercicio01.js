c769eeffec6cbfe8a600667d7e639535608e67cc
Aitor Agirre <AAgirre@uni.lan>
2019-03-13 11:39:29 +0100

    nodejs ejercicio1 for

--- nodejs/ejemplo01/ejercicio01.js
+++ nodejs/ejemplo01/ejercicio01.js
@@ -12,11 +12,33 @@ Definir los datos meteorológicos de la tendencia de los próximos 6 días. Con 
 http://www.euskalmet.euskadi.eus
 */
 
+tiempo = [
+    {dia:"Miércoles", cielo:"lluvia", temperatura:12},
+    {dia:"Jueves", cielo:"Nublado", temperatura:20},
+    {dia:"Viernes", cielo:"lluvia", temperatura:23},
+    {dia:"Sábado", cielo:"sol", temperatura:10},
+    {dia:"Domingo", cielo:"sol", temperatura:5},
+    {dia:"lunes", cielo:"Nublado", temperatura:0},
+]
 
 /*
 Imprimir los datos usando 'for' y console.log
 */
 
+/*
+console.log(tiempo[0].dia + " " + tiempo[0].cielo)
+console.log(tiempo[1].dia + " " + tiempo[1].cielo)
+console.log(tiempo[2].dia + " " + tiempo[2].cielo)
+console.log(tiempo[3].dia + " " + tiempo[3].cielo)
+console.log(tiempo[4].dia + " " + tiempo[4].cielo)
+console.log(tiempo[5].dia + " " + tiempo[5].cielo)
+*/
+
+var i;
+for (i = 0; i < tiempo.length; i++) { 
+    console.log(i)
+    console.log(tiempo[i].dia + " " + tiempo[i].cielo)
+}
 
 /*
 Crear una página HTML con los datos anteriores usando las imágenes (sol, nubes, ...)
