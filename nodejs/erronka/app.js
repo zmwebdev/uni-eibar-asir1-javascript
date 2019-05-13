578d02d40f4ab991f359ba1e76200636bceba480
Aitor Agirre <AAgirre@uni.lan>
2019-05-13 11:26:07 +0200

    erronka

--- nodejs/erronka/app.js
+++ nodejs/erronka/app.js
@@ -1,15 +1,39 @@
 // https://www.w3schools.com/nodejs/nodejs_mongodb.asp
 
+/*
+nmp init
+npm install mongodb express
+*/
+
+var express = require('express');
+var app = express();
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("mydb");
-  var myobj = { id: "1", t: "15" };
-  dbo.collection("temperatura").insertOne(myobj, function(err, res) {
+app.get('/:id/:t', function (req, res) {
+//app.get('/', function (req, res) {
+  MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
+    var dbo = db.db("erronka");
+    var myobj = { sensor_id: req.params.id, datua: req.params.t, noiz: new Date().getTime()};
+    dbo.collection("datuak").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+      //res.end();
+    });
   });
+});
+
+
+
+
+/* mongodb
+use erronka;   
+db.datuak.find();
+*/
+
+app.listen(8080, function () {
+  console.log('app running 8080!');
 });
