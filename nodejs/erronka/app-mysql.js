17d221668e1bc4d5ed19e146683c4b1292f1ebda
zmwebdev <zmwebdev@gmail.com>
2019-05-27 11:28:04 +0200

    Update app-mysql.js

--- nodejs/erronka/app-mysql.js
+++ nodejs/erronka/app-mysql.js
@@ -1,46 +1,37 @@
-// https://www.w3schools.com/nodejs/nodejs_mongodb.asp
-
-/*
-nmp init
-npm install mongodb express
-*/
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
+app.use(express.static('public'))
+
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "admin",
-  password: "admin",
-  database: "sensores"
-});
+    host: "localhost",
+    user: "root",
+    password: "admin108",
+    database: "proyecto"
+  });
 
-
-app.get('/:id/:t', function (req, res) {
-//app.get('/', function (req, res) {
-  
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "insert into datos (fecha_hora, cod_sensor, dato) VALUES (NOW()," + req.params.id + "," + req.params.t + ")";
-  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("1 record inserted");
-  });
+    else
+       console.log("Myslq connect OK!!!");
+});  
+
+app.get('/',function(req,res) {
+  //res.sendFile(__dirname + '/tiempo.html');
+  res.redirect('tiempo.html');
 });
 
-  res.end();
+app.get('/datuak', function (req, res) {
+
+    con.query("SELECT * FROM datos LIMIT 1000", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result)
+    });
 });
 
-
-
-
-/* mongodb
-use erronka;   
-db.datuak.find();
-*/
-
-app.listen(8080, function () {
-  console.log('app running 8080!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
